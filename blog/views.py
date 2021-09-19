e751548a070060332864f977d00031bc34c7e53c
Leandro Koller <lekoller@yahoo.com>
2021-09-19 17:27:45 -0300

    feat: Add article model and perform migrations.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 
-def home(request):
-    return HttpResponse('hello')
+class Home(View):
+    def get(self, request):
+        return HttpResponse('hello')
+
+    def post(self, request):
+        return HttpResponse('bye')
