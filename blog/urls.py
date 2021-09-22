9c9621507ea0d0f865af089074089254fe6a366c
Leandro Koller <lekoller@yahoo.com>
2021-09-22 17:25:46 -0300

    feat: Add templates and forms.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Article
 
 
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('articles', Article.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from django.utils import timezone
+from datetime import datetime
+
+from .models import ArticleModel
+from .forms import ArticleForm
 
 
 class Home(View):
@@ -9,3 +14,32 @@ class Home(View):
 
     def post(self, request):
         return HttpResponse('bye')
+
+
+class Article(View):
+    def get(self, request):
+        articles = ArticleModel.objects.all()
+
+        return render(request, "articles.html", {"articles": articles, "form": ArticleForm()})
+
+    def post(self, request):
+        form = ArticleForm(request.POST)
+        form.instance.created_at = datetime.now(tz=timezone.utc)
+        form.save()
+
+        return redirect('/blog/articles')
+
+        # title = request.POST['title']
+        # category = request.POST['category']
+        # author = request.POST['author']
+        # content = request.POST['content']
+
+        # ArticleModel.objects.create(
+        #     title=title,
+        #     category=category,
+        #     author=author,
+        #     content=content,
+        #     created_at=datetime.now(tz=timezone.utc)
+        # )
+
+        # return redirect('/blog/articles')
